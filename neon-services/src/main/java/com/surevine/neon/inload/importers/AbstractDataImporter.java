9a3ae0ef41ef86fc71e87bd144dfcfbb176b9c62
simonw2 <simon.white@gmail.com>
2014-02-19 18:16:57 +0000

    Now handles basic auth and SSL client certificate authentication

--- neon-services/src/main/java/com/surevine/neon/inload/importers/AbstractDataImporter.java
+++ neon-services/src/main/java/com/surevine/neon/inload/importers/AbstractDataImporter.java
@@ -4,19 +4,34 @@ import com.surevine.neon.dao.ImporterConfigurationDAO;
 import com.surevine.neon.dao.ProfileDAO;
 import com.surevine.neon.inload.DataImporter;
 import com.surevine.neon.util.DateUtil;
+
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
 
 /**
  * Abstract data importer contains all the basic functionality required for importer configuration and state management.
@@ -26,7 +41,12 @@ public abstract class AbstractDataImporter implements DataImporter {
 	private ImporterConfigurationDAO configurationDAO;
     private Logger log = Logger.getLogger(AbstractDataImporter.class);
     protected ProfileDAO profileDAO;
-
+    protected String username=null;
+    protected transient String password=null;
+    protected boolean useCertificate=false;
+    protected String keyStoreType="pkcs12";
+    private String keyStoreLocation="/home/simonw/cacerts";
+    
     /**
      * Delegated import implementation
      * @param userID the user ID to run the import for
@@ -123,6 +143,10 @@ public abstract class AbstractDataImporter implements DataImporter {
     public void setEnabled(boolean enabled) {
         configurationDAO.addImporterConfigurationOption(getImporterName(), ImporterConfigurationDAO.NS_ENABLED, Boolean.toString(enabled));
     }
+    
+    protected String getAuthorizationHeader() {
+    	return Base64.encodeBase64String(username.concat(":").concat(password).getBytes());
+    }
 
     protected String getRawWebData(String userID, String urlBase) {
 		log.info("Getting raw web content for "+userID);
@@ -132,7 +156,47 @@ public abstract class AbstractDataImporter implements DataImporter {
 		try {
 			targetURL=new URL(urlBase.replaceAll("\\{username\\}", userID));
 			log.trace("Target URL for import: "+targetURL.toString());
-			webIn = targetURL.openStream();
+			URLConnection connection = targetURL.openConnection();
+			
+			if (connection instanceof HttpsURLConnection && useCertificate) {
+				try {
+				HttpsURLConnection secureConnection = (HttpsURLConnection)connection;
+				KeyStore ksClient=KeyStore.getInstance(keyStoreType);
+				ksClient.load(new FileInputStream(keyStoreLocation), password.toCharArray());
+				
+				KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+				kmf.init(ksClient, password.toCharArray());
+				
+				SSLContext ssl = SSLContext.getInstance("TLS");
+				ssl.init(kmf.getKeyManagers(), null, null);
+				SSLSocketFactory socketFactory = ssl.getSocketFactory();
+				
+				secureConnection.setSSLSocketFactory(socketFactory);
+				}
+				catch(KeyStoreException e) {
+					log.warn("Issue handling the keystore", e);
+				}
+				catch (CertificateException e) {
+					log.warn(e);
+				}
+				catch (NoSuchAlgorithmException e) {
+					log.warn(e);
+				}
+				catch (UnrecoverableKeyException e) {
+					log.warn(e);
+				}
+				catch (KeyManagementException e) {
+					log.warn(e);
+				}
+			}
+			
+			else {
+				if (username!=null) {
+					connection.setRequestProperty("Authorization", "Basic "+getAuthorizationHeader());
+				}
+			}
+			
+			webIn = connection.getInputStream();
 			rV=IOUtils.toString(webIn);
 		}
 		catch (MalformedURLException e) {
