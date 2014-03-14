a617b50c18f4cf58a5e48c67ae9f46352e14a599
Ash Ward <ash.wardly@gmail.com>
2014-03-14 14:29:04 +0000

    Badge list now referencing baked images. Also retrieving and displaying bade metadata (such as name).

--- neon-client/src/main/webapp/js/profiles/extras/badges/init.js
+++ neon-client/src/main/webapp/js/profiles/extras/badges/init.js
@@ -1,23 +1,68 @@
-define([ 'angular', 'config' ], function(ng, config) {
-	return ng.module('profiles.extras.badges', []).controller(
-			'profilesBadgesExtrasCtrl',
-			[
-					'$scope',
-					'$http',
-					function($scope, $http) {
+define(
+		[ 'angular', 'config' ],
+		function(ng, config) {
+			return ng
+					.module('profiles.extras.badges', [])
+					.controller(
+							'profilesBadgesExtrasCtrl',
+							[
+									'$scope',
+									'$http',
+									function($scope, $http) {
 
-						$scope.badges = {
-							hasBadges : false,
-							list : []
-						}
+										$scope.badges = {
+											hasBadges : false,
+											list : []
+										}
 
-						$scope.$watch('profile.userID', function(
-								newVal, oldVal) {
-							$http({method: 'GET', url: config.restBaseUrl + 'badges/assertion/list/' + escape(newVal)})
-							.success(function(data) {
-								$scope.badges.list = data;
-								$scope.badges.hasBadges = ( data.length > 0 );
-							});
-						});
-					} ]);
-});
+										$scope
+												.$watch(
+														'userId',
+														function(newVal, oldVal) {
+															$http(
+																	{
+																		method : 'GET',
+																		url : config.restBaseUrl
+																				+ 'badges/assertion/list/'
+																				+ escape(newVal)
+																	})
+																	.success(
+																			function(
+																					data) {
+																				$scope.badges.hasBadges = (data.length > 0);
+
+																				$scope.badges.list = [];
+
+																				for ( var i = 0; i < data.length; ++i) {
+																					var assertion = data[i];
+
+																					var displayBadge = {
+																						assertionData : assertion,
+																						badgeData : null,
+																						bakedUrl : config.restBaseUrl
+																								+ 'badges/bake/'
+																								+ escape(assertion.namespace),
+																					};
+
+																					if (assertion.image) {
+																						displayBadge.bakedUrl += '?source='
+																								+ escape(assertion.image);
+																					}
+
+																					$http(
+																							{
+																								method : 'GET',
+																								url : assertion.badge
+																							})
+																							.success(
+																									function(
+																											badgeData) {
+																										displayBadge.badgeData = badgeData;
+																									});
+																					
+																					$scope.badges.list.push(displayBadge);
+																				}
+																			});
+														});
+									} ]);
+		});
