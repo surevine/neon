75c656245813c14c18e69cd70037ae2332275f80
Nick <nick.leaver@surevine.com>
2014-03-20 13:47:38 +0000

    added additional services for system health

--- neon-services/src/main/java/com/surevine/neon/service/InloadControlService.java
+++ neon-services/src/main/java/com/surevine/neon/service/InloadControlService.java
@@ -2,6 +2,7 @@ package com.surevine.neon.service;
 
 import com.surevine.neon.service.bean.ImporterConfigurationServiceBean;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -21,6 +22,12 @@ public interface InloadControlService {
      * @return configuration bean with settings for the named importer
      */
     public ImporterConfigurationServiceBean getConfigForImporter(String importer);
+
+    /**
+     * Gets the configuration for all importers
+     * @return the configuration for all importers
+     */
+    public Collection<ImporterConfigurationServiceBean> getAllImporterConfigurations();
 
     /**
      * Updates or sets configuration options for an importer. Does not remove any existing config - can only add or 
